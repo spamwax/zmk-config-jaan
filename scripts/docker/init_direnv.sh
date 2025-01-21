d290a9a503d7c1e97a1572c600b6da56bfebd48e
Hamid Ghadyani <hamid@hamid.cc>
2025-01-21 04:00:36 -0500

    (chore): docker related changes.

--- scripts/docker/init_direnv.sh
+++ scripts/docker/init_direnv.sh
@@ -6,5 +6,5 @@ source ~/.bashrc >/dev/null 2>&1
 cd /root/zmk-workspace || exit
 
 eval "$(direnv export bash)"
-direnv exec . just init >/dev/null 2>&1
+direnv exec . just init # >/dev/null 2>&1
 # direnv exec . just build all
